50f3cf21ece1ef2b7c1e9e40a80cbdfc5773a4ab
15291967179@163.com <15291967179@163.com>
2020-06-16 10:20:29 +0800

    11111

---
+++ app/src/main/java/com/beitone/signup/ui/account/ImproveInformationActivity.java
@@ -0,0 +1,16 @@
+package com.beitone.signup.ui.account;
+
+import com.beitone.signup.base.BaseActivity;
+
+public class ImproveInformationActivity extends BaseActivity {
+
+    @Override
+    protected int getContentViewLayoutId() {
+        return 0;
+    }
+
+    @Override
+    protected void initViewAndData() {
+
+    }
+}
--- app/src/main/java/com/beitone/signup/ui/account/RegisterActivity.java
+++ app/src/main/java/com/beitone/signup/ui/account/RegisterActivity.java
@@ -1,10 +1,17 @@
 package com.beitone.signup.ui.account;
 
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.TextPaint;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import androidx.annotation.NonNull;
 
 import com.beitone.signup.R;
 import com.beitone.signup.base.BaseActivity;
@@ -31,6 +38,8 @@ public class RegisterActivity extends BaseActivity {
     AppButton btnNext;
     @BindView(R.id.tvLogin)
     TextView tvLogin;
+    @BindView(R.id.tvAgree)
+    TextView tvAgree;
 
     @Override
     protected int getContentViewLayoutId() {
@@ -41,6 +50,27 @@ public class RegisterActivity extends BaseActivity {
     protected void initViewAndData() {
         ButterKnife.bind(this);
         setTitle("注册");
+
+        SpannableString spannableString = new SpannableString(getString(R.string.register_login));
+        AgreeClickSpan agreeClickSpan = new AgreeClickSpan("login");
+        spannableString.setSpan(agreeClickSpan, 5, spannableString.length(), Spanned.SPAN_INCLUSIVE_EXCLUSIVE);
+        tvLogin.setMovementMethod(LinkMovementMethod.getInstance());
+        tvLogin.setHighlightColor(getResources().getColor(android.R.color.transparent));
+        tvLogin.setText(spannableString);
+
+
+        spannableString = new SpannableString(getString(R.string.register_agree));
+        agreeClickSpan = new AgreeClickSpan("agree1");
+        spannableString.setSpan(agreeClickSpan, 4, 8, Spanned.SPAN_INCLUSIVE_EXCLUSIVE);
+
+        agreeClickSpan = new AgreeClickSpan("agree2");
+        spannableString.setSpan(agreeClickSpan, 11, spannableString.length(), Spanned.SPAN_INCLUSIVE_EXCLUSIVE);
+        tvAgree.setMovementMethod(LinkMovementMethod.getInstance());
+        tvAgree.setHighlightColor(getResources().getColor(android.R.color.transparent));
+        tvAgree.setText(spannableString);
+
+
+
     }
 
 
@@ -57,4 +87,32 @@ public class RegisterActivity extends BaseActivity {
                 break;
         }
     }
+
+
+
+    class AgreeClickSpan extends ClickableSpan {
+
+        private String action;
+
+        public AgreeClickSpan(String action) {
+            this.action = action;
+        }
+
+        @Override
+        public void updateDrawState(TextPaint ds) {
+            super.updateDrawState(ds);
+            ds.setColor(getResources().getColor(R.color.colorAccent));//设置颜色
+            ds.setUnderlineText(false);//去掉下划线
+        }
+
+        @Override
+        public void onClick(@NonNull View view) {
+            if (action.equals("login")) {
+                finish();
+            } else {
+
+            }
+        }
+    }
+
 }
